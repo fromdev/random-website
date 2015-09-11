88020ddfc6030c8b38f906740eba2fce9ae30b12
Sachin Joshi <software.wikipedia@gmail.com>
2015-09-11 12:15:56 -0700

    added js code to show list automaticaly
    automated the list show

--- display-sites-list.js
+++ display-sites-list.js
@@ -4,3 +4,18 @@ var addToRandomSupportedSites = function(arr) {
 		randomSupportedSites.push(arr[i]);
 	}
 };
+$(window).load(function(){
+	var cList = $('ul.supporteSites')
+	$.each(randomSupportedSites, function(i)
+{
+    var li = $('<li/>')
+        .addClass('ui-menu-item')
+        .attr('role', 'menuitem')
+        .appendTo(cList);
+    var aaa = $('<a/>')
+        .addClass('ui-all')
+        .text(randomSupportedSites[i])
+        .appendTo(li);
+});
+	
+});
