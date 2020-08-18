acd2a9408f9d136a6e67ff2b770bbd588afcd680
Sachin Joshi <software.wikipedia@gmail.com>
2020-08-18 13:30:47 -0700

    boredbutton
    boredbutton

--- random.supported.sites.js
+++ random.supported.sites.js
@@ -1,8 +1,8 @@
 addToRandomSupportedSites(
 [
 		'https://en.wikipedia.org/wiki/Special:RandomInCategory/Computer_programming',
-		'https://www.wikihow.com/Special:Randomizer',
 		'https://www.fromdev.com/random',
-		'https://www.discuvver.com/jump2.php'
+		'https://www.discuvver.com/jump2.php',
+		'https://www.boredbutton.com/random'
 ]
 );
