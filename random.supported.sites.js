3c083542fc07acc02f13df92bdccf8c822b61919
Sachin Joshi <software.wikipedia@gmail.com>
2020-08-18 13:28:00 -0700

    trying discuvver

--- random.supported.sites.js
+++ random.supported.sites.js
@@ -2,6 +2,7 @@ addToRandomSupportedSites(
 [
 		'https://en.wikipedia.org/wiki/Special:RandomInCategory/Computer_programming',
 		'https://www.wikihow.com/Special:Randomizer',
-		'https://www.fromdev.com/random'
+		'https://www.fromdev.com/random',
+		'https://www.discuvver.com/jump2.php'
 ]
 );
